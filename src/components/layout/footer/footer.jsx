253a9dfbb98db213b30bdc0b65a8ad612d54d03c
javohir1606 <javohirallayorov1606@gmail.com>
2024-10-14 16:47:43 +0500

    main

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -1,6 +1,6 @@
 import { Box, Container, Stack, Typography } from "@mui/material";
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
--- src/components/layout/header/components/style.js
+++ src/components/layout/header/components/style.js
@@ -1,5 +1,4 @@
 import styled from "@emotion/styled";
-import { Stack, TextField } from "@mui/material";
 import { Link } from "react-router-dom";
 export const LogoLink = styled(Link) `
 max-width: 220px;
