d8bcf685a66826034c6ece650ed5866cd3ccacdf
javohir1606 <javohirallayorov1606@gmail.com>
2024-10-14 13:42:46 +0500

    main

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -1,6 +1,34 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box bgcolor={"#F2F2F2"}>
+      <Container maxWidth="xs">
+        <Stack  py={"40px"} direction={"row"} gap={"64px"}>
+        <Stack>
+          <Typography variant="body1" mb={"24px"}>Каталог товаров</Typography>
+          <Typography mb={"16px"} color="grey">Ковры</Typography>
+          <Typography mb={"16px"} color="grey">Коврики</Typography>
+          <Typography mb={"16px"} color="grey">Дорожки</Typography>
+          <Typography mb={"16px"} color="grey">Для ванной</Typography>
+          <Typography mb={"16px"} color="grey">Особенные ковры</Typography>
+        </Stack>
+        <Stack>
+          <Typography variant="body1" mb={"24px"}>Личный кабинет</Typography>
+          <Typography mb={"16px"} color="grey">Личный кабинет</Typography>
+          <Typography mb={"16px"} color="grey">Мои заказы</Typography>
+          <Typography mb={"16px"} color="grey">Избранное</Typography>
+        </Stack>
+        <Stack>
+          <Typography variant="body1" mb={"24px"}>Центр поддержки</Typography>
+          <Typography mb={"16px"} color="grey">Контакты</Typography>
+          <Typography mb={"16px"} color="grey">Доставка</Typography>
+          <Typography mb={"16px"} color="grey">Возвраты</Typography>
+        </Stack>
+        </Stack>
+      </Container>
+    </Box>
+  );
 };
-
